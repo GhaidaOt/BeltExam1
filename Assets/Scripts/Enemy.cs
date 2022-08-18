3cbdcf2bd429a898208961751813a971053f89cd
Ghaida Alothman <61880998+GhaidaOt@users.noreply.github.com>
2022-08-18 13:46:12 +0300

    almost there

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform[] points;
+    int current;
+    public float speed;
+
     void Start()
     {
-        
+        current = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position != points[current].position)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, points[current].position, speed * Time.deltaTime);
+        }
+        else
+        {
+            current = (current + 1) % points.Length;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
